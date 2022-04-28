8552d298fdc981d2b7d8ae3f4df37f3b15aaa0ea
ItsEjaz <45075795+ItsEjaz@users.noreply.github.com>
2022-04-28 19:34:24 -0400

    add new changes

--- src/App.js
+++ src/App.js
@@ -1,82 +1,69 @@
-
-import robot from './Components/robot.png';
 import './App.css';
-import Profile from './Components/Profile'
+import Profile from './Components/Profile';
 
-import react,{useState,useEffect} from 'react';
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [Data,setData]=useState([]);
-  const [search,setSearch]=useState('');
-  const [searchResult,setSearchResults] = useState("");
+	const [data, setData] = useState([]);
+	const [search, setSearch] = useState('');
+	const [searchResult, setSearchResults] = useState('');
 
- useEffect(()=>{
-   axios.get('https://jsonplaceholder.typicode.com/users')
-   .then(response => {
-     setData(response.data);
-     
-    });
- },[]) ;
+	useEffect(() => {
+		axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+			setData(response.data);
+		});
+	}, []);
 
- const dataFilter =(e)=>{
-  setSearch(e.target.value)
-   if (search !== '') {
-     const filterSearch = Data.filter((item) => {
-       return Object.values(item).join('').toLowerCase().
-       includes(search.toLowerCase())})
-       setSearchResults(filterSearch)
-     }
-     else{
-      setSearchResults(Data)
-     }
-   }
- 
+	const dataFilter = (e) => {
+		setSearch(e.target.value);
+		if (search !== '') {
+			const filterSearch = data.filter((item) => {
+				return Object.values(item)
+					.join('')
+					.toLowerCase()
+					.includes(search.toLowerCase());
+			});
+			setSearchResults(filterSearch);
+		} else {
+			setSearchResults(data);
+		}
+	};
 
-  return (
-    <div className="App">
-     
-      <h1 className="App-logo">
-        ROBO APP
-      </h1>
+	return (
+		<div className="App">
+			<h1 className="App-logo">ROBO APP CENTER</h1>
 
+			<input
+				class="search"
+				value={search}
+				placeholder="Search..."
+				onChange={dataFilter}
+			></input>
 
-
-      <input 
-      class='search' 
-      value={search}
-      placeholder="Search..."
-      onChange={e=>dataFilter(e)}
-      ></input>
-      
-      <div className="Card-Container">
-        {search.length>1 ? (searchResult.map((item) => {
-          return(
-          <Profile 
-          picture={
-            "https://robohash.org/"+item.id
-          } 
-          name={item.name} 
-          email={item.email} />
-          )})) : (
-            Data.map((item) => {
-            return(
-            <Profile 
-            picture={
-              "https://robohash.org/"+item.id
-            }
-            name={item.name} 
-            email={item.email} />
-            )
-        }
-        
-        ))}
-                
-      </div>
-      
-
-    </div>
-  );
+			<div className="Card-Container">
+				{search.length > 1
+					? searchResult.map((item) => {
+							return (
+								<Profile
+									picture={'https://robohash.org/' + item.id}
+									name={item.name}
+									email={item.email}
+								/>
+							);
+					  })
+					: data.map((item) => {
+							return (
+								<Profile
+									picture={'https://robohash.org/' + item.id}
+									name={item.name}
+									email={item.email}
+								/>
+							);
+					  })}
+			</div>
+		</div>
+	);
 }
 
 export default App;
